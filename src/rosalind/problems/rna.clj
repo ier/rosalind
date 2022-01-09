6cf0c6432d27ec3c80feab07ff48cf743a050c02
ier <ivan.erakhtin@gmail.com>
2022-01-09 22:54:51 +0300

    Add 'rna' solution

---
+++ src/rosalind/problems/rna.clj
@@ -0,0 +1,15 @@
+(ns rosalind.problems.rna
+  (:require
+   [clojure.string :as str]))
+
+
+(defn solve-rna
+  [s]
+  (-> s
+      (str/replace "T" "U")
+      (str/replace "\n" "")))
+
+
+(->> "resources/inputs/rosalind_rna.txt"
+     slurp
+     solve-rna)
