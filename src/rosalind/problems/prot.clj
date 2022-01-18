2624b7c3d42db2c09de750434b0ce888045add02
ier <ivan.erakhtin@gmail.com>
2022-01-19 00:29:00 +0300

    Add 'prot' solution

---
+++ src/rosalind/problems/prot.clj
@@ -0,0 +1,40 @@
+(ns rosalind.problems.prot
+  (:require
+   [clojure.string :as str]))
+
+
+(def rna-codon-table 
+  {"UUU" "F"      "CUU" "L"      "AUU" "I"      "GUU" "V"
+   "UUC" "F"      "CUC" "L"      "AUC" "I"      "GUC" "V"
+   "UUA" "L"      "CUA" "L"      "AUA" "I"      "GUA" "V"
+   "UUG" "L"      "CUG" "L"      "AUG" "M"      "GUG" "V"
+   "UCU" "S"      "CCU" "P"      "ACU" "T"      "GCU" "A"
+   "UCC" "S"      "CCC" "P"      "ACC" "T"      "GCC" "A"
+   "UCA" "S"      "CCA" "P"      "ACA" "T"      "GCA" "A"
+   "UCG" "S"      "CCG" "P"      "ACG" "T"      "GCG" "A"
+   "UAU" "Y"      "CAU" "H"      "AAU" "N"      "GAU" "D"
+   "UAC" "Y"      "CAC" "H"      "AAC" "N"      "GAC" "D"
+   "UAA" ""       "CAA" "Q"      "AAA" "K"      "GAA" "E"
+   "UAG" ""       "CAG" "Q"      "AAG" "K"      "GAG" "E"
+   "UGU" "C"      "CGU" "R"      "AGU" "S"      "GGU" "G"
+   "UGC" "C"      "CGC" "R"      "AGC" "S"      "GGC" "G"
+   "UGA" ""       "CGA" "R"      "AGA" "R"      "GGA" "G"
+   "UGG" "W"      "CGG" "R"      "AGG" "R"      "GGG" "G"})
+
+
+(defn- prot
+  [s]
+  (->> s
+       (partition 3)
+       (map #(reduce str %))
+       (map rna-codon-table)
+       (reduce str)))
+
+
+(defn solve-prot [s]
+  (-> s
+      (str/replace "\n" "")
+      prot))
+
+
+(solve-prot (slurp "resources/inputs/rosalind_prot.txt"))
